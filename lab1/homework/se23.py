55d8d982227dbf8cb475ea5429e2e97bfd7914a2
nmhoangg2000 <45127849+nmhoangg2000@users.noreply.github.com>
2018-12-11 14:46:09 +0700

    Update se23.py

--- lab1/homework/se23.py
+++ lab1/homework/se23.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 uri = "mongodb://admin:admin@ds021182.mlab.com:21182/c4e"
 client = MongoClient(uri)
 db = client.get_database()
-post_collection = db["post"]
+post_collection = db["posts"]
 new_document ={
     "title":"???",
     "authur": "HOÀNG",
